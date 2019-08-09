c5fb553ce1c2a3b8f23f5d5406c2779e44a126c2
yippira <yippygames@gmail.com>
2019-08-09 19:29:22 +0800

    try

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'yippira'`, () => {
+  it(`should have as title 'yippira.github.io'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('yippira');
+    expect(app.title).toEqual('yippira.github.io');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to yippira!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to yippira.github.io!');
   });
 });
