25a3d8e0d6b14ac33d86211bb681511553f929ef
yippira <yippygames@gmail.com>
2019-08-09 19:56:12 +0800

    urlPrefix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  environment = environment;
   constructor() { }
 
   ngOnInit() {
