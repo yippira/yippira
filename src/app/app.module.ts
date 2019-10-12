bdb3a99ef8e85a651c2e7c6f76d3c8d4f3b5b307
pereira yip <yippygames@gmail.com>
2019-10-12 18:49:11 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,18 +8,21 @@ import { MglTimelineModule } from 'angular-mgl-timeline';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
-import {MatToolbarModule, MatButtonModule} from '@angular/material';
+import { MatToolbarModule, MatButtonModule } from '@angular/material';
 import { PersonalStatementComponent } from './personal-statement/personal-statement.component';
 import { LatestProjectsComponent } from './latest-projects/latest-projects.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { GeneticAlgoShowcaseComponent } from './genetic-algo-showcase/genetic-algo-showcase.component';
 import { ExperienceTimelineComponent } from './experience-timeline/experience-timeline.component';
-
+import { FooterComponent } from './footer/footer.component';
+import { StickyNavComponent } from './sticky-nav/sticky-nav.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import {HttpClientModule} from '@angular/common/http';
 const material = [
   MatToolbarModule,
   MatButtonModule
 ]
-  
+
 
 
 @NgModule({
@@ -32,15 +35,21 @@ const material = [
     LatestProjectsComponent,
     TimelineComponent,
     GeneticAlgoShowcaseComponent,
-    ExperienceTimelineComponent
+    ExperienceTimelineComponent,
+    FooterComponent,
+    StickyNavComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MglTimelineModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     material
-     
+
   ],
   exports: [material],
   providers: [],
